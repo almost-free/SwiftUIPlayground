5f4c546d11afb42dc30342b3c09c2a0440743305
Jonathan Willis <jondwillis@gmail.com>
2019-06-06 15:36:24 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+SwiftUI
+===
+
+I'm just seeing how SwiftUI might work out in some kinda-sorta-realistic use case.
+
+Overall this will be useful and promote better design patterns.
+
+Some downsides are a decent amount of boilerplate relating to state, and that it will be non-trivial to backport.
+
+Adapted from https://mecid.github.io/2019/06/05/swiftui-making-real-world-app/
---
+++ SwiftUIPlayground/ContentView.swift
@@ -0,0 +1,91 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayground
+//
+//  Created by Jonathan Willis on 6/6/19.
+//  Copyright © 2019 Almost Free. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+struct ContentView : View {
+
+    struct ViewModel {
+        var subHeader = "Hello Other World"
+    }
+
+    class ViewStore: BindableObject {
+        var didChange = PassthroughSubject<ViewStore, Never>()
+
+        let service: ViewService
+
+        public init(service: ViewService) {
+            self.service = service
+        }
+
+        var header = "Hello From:" {
+            didSet {
+                didChange.send(self)
+            }
+        }
+
+        var subHeader = "The World" {
+            didSet {
+                didChange.send(self)
+            }
+        }
+
+        var buttonText = "Press Me" {
+            didSet {
+                didChange.send(self)
+            }
+        }
+
+        func doTheThing() {
+            subHeader = "???"
+            buttonText = "Pressing…"
+
+            service.doTheThing { result in
+                switch result {
+                case .success(let subHeader):
+                    self.subHeader = subHeader
+                    self.buttonText = "You pressed me!"
+                case .failure(let error):
+                    self.subHeader = error.localizedDescription
+                }
+            }
+        }
+    }
+
+    class ViewService {
+        func doTheThing(_ handler: @escaping ((Result<String, Error>) -> Void)) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                handler(.success("The Other Side"))
+            }
+        }
+    }
+
+    @State var viewModel = ViewModel()
+    @EnvironmentObject var store: ViewStore
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text(store.header)
+                Text(store.subHeader)
+            }
+            Button(action: store.doTheThing) {
+                Text(store.buttonText)
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
